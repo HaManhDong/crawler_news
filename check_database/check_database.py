56171952a7bffef26ccfd397b08936ee259d52aa
haminhcong <haminhcongbkhn@gmail.com>
2016-11-24 10:33:39 +0700

    update

--- check_database/check_database.py
+++ check_database/check_database.py
@@ -1,4 +1,6 @@
 import database_connection
 la_time_session = database_connection.create_database_and_connect('latimes')
-kss= la_time_session.query(database_connection.NewsData).first()
+
+first_entry= la_time_session.query(database_connection.NewsData).first()
+print first_entry.content
 pass
